de8bf7a35cd687df1a3f2846e9cea6b344c17bc5
Flavio Rozendo <flavio.ap.rozendo@gmail.com>
2021-09-07 18:08:30 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from "components/data-table";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Helloworld</h1>
+
+      <DataTable />
+
     </div>
+    <Footer />
+    </>
   );
 }
 
